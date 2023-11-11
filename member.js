6fe3878dfb050f2b2870a3b68adff4567c233e68
muhimin <62156192+muhimin01@users.noreply.github.com>
2023-11-11 02:49:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,65 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["js", "html", "css"],
+        salary: 1000,
+        bonus: 200,
+        getSalary: function () {
+            return this.salary + this.bonus;
+        }
+    };
+    member.getSalary();
+    member.skills.push("php");
+    member.skills.push("java");
+    member.skills.push("python");
+    member.skills.push("ruby");
+    member.skills.push("c");
+    member.skills.push("c++");
+    member.skills.push("c#");
+    member.skills.push("scala");
+    member.skills.push("swift");
+    member.skills.push("kotlin");
+    member.skills.push("go");
+    member.skills.push("rust");
+    member.skills.push("typescript");
+    member.skills.push("dart");
+    member.skills.push("r");
+    member.skills.push("perl");
+    member.skills.push("haskell");
+    member.skills.push("assembly");
+    member.skills.push("cobol");
+    member.skills.push("fortran");
+    member.skills.push("lisp");
+    member.skills.push("scheme");
+    member.skills.push("prolog");
+    member.skills.push("sql");
+    member.skills.push("pl/sql");
+    member.skills.push("t-sql");
+    member.skills.push("pascal");
+    member.skills.push("erlang");
+    member.skills.push("elixir");
+    member.skills.push("lua");
+    member.skills.push("clojure");
+    member.skills.push("racket");
+    member.skills.push("julia");
+    member.skills.push("f#");
+    member.skills.push("ocaml");
+    member.skills.push("bash");
+    member.skills.push("powershell");
+    member.skills.push("awk");
+    member.skills.push("sed");
+    member.skills.push("ksh");
+    member.skills.push("zsh");
+    member.skills.push("fish");
+    member.skills.push("hcl");
+    member.skills.push("terraform");
+    member.skills.push("hcl");
+    member.skills.push("yaml");
+    member.skills.push("json");
+    member.skills.push("xml");
+    member.skills.push("xslt");
+    member.skills.push("xquery");
+    member.skills.push("xpath");
+    member.skills.push("xquery");
+}
